9d35e0558789972ccfdd2babe592bc1dee0f58e9
Sondshy <95878028+Sondshy@users.noreply.github.com>
2021-12-09 22:13:11 +0300

    commmand.js

---
+++ config.js
@@ -0,0 +1,62 @@
+const config = {
+"botStatus": ["HaylazUnity ❤️ $ond.", "HaylazUnity ❤️ $ond.", "HaylazUnity ❤️ $ond."],
+"botOwners": ["809474502802800662"],
+"guildID": "908781565444767784",
+"tag": "ਮ",
+"guildURL": "rvt",
+"channels": {
+    "botVoiceChannel": "917454664033763348",
+    "generalChat": "917441044189159514",
+    "registerChat": "917436223881089025",
+    "jailLogs": "918516374886817823",
+    "chatMuteLogs": "918516374886817823",
+    "voiceMuteLogs": "918516374886817823",
+    "banLogs": "918516374886817823",
+    "adsLogs": "918516374886817823",
+    "moderationLogs": "918516374886817823",
+    "streamPunitiveLogs": "918516374886817823",
+    "bannedTagLogs": "918516374886817823",
+    "streamerDenetimLog": "918516374886817823",
+    "tagRemoveLog": "918516374886817823",
+    "authyLeaveLog": "918516374886817823",
+    "leaveFamilyLog": "918516374886817823",
+    "joinFamilyLog": "918516374886817823",
+    "commandBlockLog": "918516374886817823",
+    "giveManageRoleLog": "918516374886817823",
+    "unbanLogs": "918516374886817823",
+    "basicNicknameLog": "918516374886817823"
+},
+"roles": {
+    "moveAuth": [], // Taşıma yetkileri
+    "registerAuth": [], // Register Yetkileri
+    "banAuth": [],  // Ban yetkileri
+    "jailAuth": [], // Jail yetkileri
+    "chatMuteAtuh": [], // CMute yetkileri
+    "voiceMuteAuth": ["",], // VMute yetkilieri
+    "unmuteAuth": [], // Unmute Yetkileri
+    "managementRoles": ["917375303658123274", "917375291251392523", "917375291062636544", "917375297366667304",], // Üst yönetim rolleri
+    "manRoles": ["909175759359324252"], // Erkek rolleri
+    "womanRoles": ["909228031187492914"], // Kadın rolleri
+    "unregisterRoles": ["917471305115238452"], // Kayıtsız roller,
+    "warnRoleOne": "918518603802882088", // Uyarı 1
+    "warnRoleTwo": "918518612032118875", // Uyarı 2
+    "warnRoleThree": "918518613391048754", // Uyarı 3
+    "karantinaRole": "918147680117612554", // Karantina Rolü
+    "suspectedRole": "918147680117612554", // Şüpheli Rolü
+    "familyRole": "909223599230042172", // Taglı rolü
+    "bannedTagRole": "918147680117612554", // Yasaklı Tag Rolü
+    "authySayRole": ["917375303658123274", "917375291251392523", "917375291062636544", "917375297366667304", "917375324247982150"], // Yetkili say rolleri
+    "boosterRole": "915622853334937600", // Booster Rolü
+    "botCommandRole": "918517900782993408", // Bot Komut Rolü
+},
+"moderatorRole": {
+    "oneManageRole": "917369887242989568", // İlk yetkili rolü
+    "twoManageRole": "917369855890554910", // İkinci yetkili rolü
+},
+"member":{
+    "guildOwners": ["657648013493403658", "809474502802800662",]
+}
+
+}
+
+module.exports = config;
---
+++ keys.js
@@ -0,0 +1,20 @@
+const keys = {
+    "token": "OTE4NTIyNDk4MzQ0MTg5OTYy.YbIevA.50GBVyywddA18VlqE1rb7P5ASiE",
+    "mongoose": "",
+    "hookLogs":{
+    "discordUserLogID": "918516374886817823",
+    "discordUserLogToken": "918516374886817823",
+    "voiceLogID": "918516374886817823",
+    "voiceLogToken": "918516374886817823",
+    "messageLogID": "918516374886817823",
+    "messageLogToken": "918516374886817823",
+    "boostLogID": "918516374886817823",
+    "boostLogToken": "918516374886817823",
+    "commandLogID": "918516374886817823",
+    "commandLogToken": "918516374886817823",
+    "nameChangeLog": "918516374886817823",
+    "nameChangeLogToken": "918516374886817823"
+    }
+};
+
+module.exports = keys;
